d2b44e3fb8bf80056a53c6f678225f37e872b41b
Benny Powers <web@bennypowers.com>
2020-02-12 16:04:04 +0200

    fix: clean up the StatePredicate types

--- src/logic.ts
+++ src/logic.ts
@@ -1,19 +1,19 @@
-export type StatePredicate<X, Y, Z> = (x: X, y: Y, z: Z) => boolean;
+export type StatePredicate<X, Z> = (x: X, y: X, z: Z) => boolean;
 
-export const when = <X, Y, Z, Fx>(p: StatePredicate<X, Y, Z>, f: (x: X, y: Y, z: Z) => Fx) =>
-  (x: X, y: Y, z: Z): X|Fx =>
+export const when = <X, Z, Fx>(p: StatePredicate<X, Z>, f: (x: X, y: X, z: Z) => Fx) =>
+  (x: X, y: X, z: Z): X|Fx =>
     p(x, y, z) ? f(x, y, z) : x;
 
-export const not = <X, Y, Z>(p: StatePredicate<X, Y, Z>) =>
-  (x: X, y: Y, z: Z): boolean =>
+export const not = <X, Z>(p: StatePredicate<X, Z>) =>
+  (x: X, y: X, z: Z): boolean =>
     !p(x, y, z);
 
-export const and = <X, Y, Z>(p: StatePredicate<X, Y, Z>, q: StatePredicate<X, Y, Z>) =>
-  (x: X, y: Y, z: Z): boolean =>
+export const and = <X, Z>(p: StatePredicate<X, Z>, q: StatePredicate<X, Z>) =>
+  (x: X, y: X, z: Z): boolean =>
     p(x, y, z) &&
     q(x, y, z);
 
-export const or = <X, Y, Z>(p: StatePredicate<X, Y, Z>, q: StatePredicate<X, Y, Z>) =>
-  (x: X, y: Y, z: Z): boolean =>
+export const or = <X, Z>(p: StatePredicate<X, Z>, q: StatePredicate<X, Z>) =>
+  (x: X, y: X, z: Z): boolean =>
     p(x, y, z) ||
     q(x, y, z);
